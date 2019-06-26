38a3f2bec396c874655bddbcbe95eea2a42909ee
Enrique Bermúdez <enbermudev@gmail.com>
2019-06-25 22:00:46 -0430

    feat: add name and extra fields in every generated error

--- index.js
+++ index.js
@@ -2,29 +2,26 @@ const httpStatus = require("http-status");
 
 const env = process.env.NODE_ENV === "production";
 
-const failExpress = options => {
+const failExpress = (options = {}) => {
   // eslint-disable-next-line no-unused-vars
   return function handler(err, req, res, next) {
     let status = err.status || err.statusCode || 500;
     if (status < 400) status = 500;
 
+    const name = httpStatus[status];
     const message = httpStatus[`${status}_MESSAGE`];
 
-    const body = { status, message };
+    const body = { name, status, message };
 
     if (env === "production" || options.stackTrace) body.stack = err.stack;
 
-    if (status >= 500) {
-      console.error(err.stack); // eslint-disable-line no-console
-      return res.status(status).send(body);
-    }
-
     if (err.code) body.code = err.code;
-    if (err.name) body.name = err.name;
     if (err.type) body.type = err.type;
+
+    if (status >= 500) console.error(err.stack); // eslint-disable-line no-console
 
     return res.status(status).send(body);
   };
 };
 
-export default failExpress;
+module.exports = failExpress;
