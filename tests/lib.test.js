5b4a887eb196831c252a1389c0086097738cbd6b
Enrique Bermúdez <enbermudev@gmail.com>
2019-06-29 13:13:20 -0430

    test: create the express app inside each different test

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,5 +1,7 @@
 const request = require('supertest');
-const app = require('./server');
+const express = require('express');
+const createError = require('http-errors');
+const failExpress = require('../index.js');
 
 const consoleError = console.error;
 
@@ -8,6 +10,12 @@ afterAll(() => console.error = consoleError);
 
 describe('Test the middleware', () => {
   test('It should not return an error if everything goes right', async (done) => {
+    let app = express();
+
+    app.get('/success', (req, res) => res.send({ message: 'Success' }));
+
+    app.use(failExpress());
+
     request(app)
       .get('/success')
       .expect(200)
@@ -18,6 +26,12 @@ describe('Test the middleware', () => {
   });
 
   test('It should return and 404 error', async (done) => {
+    let app = express();
+
+    app.get('/error/404', (req, res, next) => next(createError(404, 'Not Found')));
+
+    app.use(failExpress());
+
     request(app)
       .get('/error/404')
       .expect(404)
@@ -27,7 +41,18 @@ describe('Test the middleware', () => {
       })
   });
 
-  test('It should return and 502 error', async (done) => {
+  test('It should return and 502 error with code and type', async (done) => {
+    let app = express();
+
+    app.get('/error/502', (req, res, next) => {
+      next(createError(502, 'Bad Gateway', {
+        code: 'Error code',
+        type: 'Error type',
+      }));
+    });
+
+    app.use(failExpress());
+
     request(app)
       .get('/error/502')
       .expect(502)
@@ -35,6 +60,26 @@ describe('Test the middleware', () => {
         expect(res.body.name).toEqual('Bad Gateway');
         expect(res.body.code).toEqual('Error code');
         expect(res.body.type).toEqual('Error type');
+        return done(err);
+      })
+  });
+
+  test('It should expose additional properties passed through configuration', async (done) => {
+    let app = express();
+
+    app.get('/error/403', (req, res, next) => {
+      next(createError(403, 'Forbidden', { foo: 'bar' }));
+    });
+
+    app.use(failExpress({ exposeAdditionalProperties: true }));
+
+    request(app)
+      .get('/error/403')
+      .expect(403)
+      .end((err, res) => {
+        expect(res.body.name).toEqual('Forbidden');
+        expect(res.body.status).toEqual(403);
+        expect(res.body.foo).toEqual('bar');
         return done(err);
       })
   });
