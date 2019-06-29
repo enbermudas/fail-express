e45822b32b8c7d659d60f1708fa60c06c2e3e62a
Enrique Bermúdez <enbermudev@gmail.com>
2019-06-29 13:12:30 -0430

    feat: add 'expose additional properties' option to the middleware

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const httpStatus = require("http-status");
 
-const env = process.env.NODE_ENV === "production";
+const env = process.env.NODE_ENV || "production";
 
 const failExpress = (options = {}) => {
   // eslint-disable-next-line no-unused-vars
@@ -13,12 +13,22 @@ const failExpress = (options = {}) => {
 
     const body = { name, status, message };
 
-    if (env === "production" || options.stackTrace) body.stack = err.stack;
+    if (env === "production" || options.displayStackTrace) {
+      body.stack = err.stack;
+    }
 
     if (err.code) body.code = err.code;
     if (err.type) body.type = err.type;
 
     if (status >= 500) console.error(err.stack); // eslint-disable-line no-console
+
+    if (options.exposeAdditionalProperties) {
+      Object.keys(err).forEach(key => {
+        if (!(key in body)) {
+          body[key] = err[key];
+        }
+      });
+    }
 
     return res.status(status).send(body);
   };
